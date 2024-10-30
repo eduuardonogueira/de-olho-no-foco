9824d4fa6c1dc2a16d93451b5f35b4804fe33eef
Eduardo Nogueira de Castro <eduardocastroemp@gmail.com>
2024-10-30 17:59:23 -0300

    added: map location

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,1 +1,3 @@
 export const MAP_ROUTE = '/'
+export const LOGIN_ROUTE = '/login'
+export const SIGNUP_ROUTE = '/signup'
--- src/router.tsx
+++ src/router.tsx
@@ -1,17 +1,20 @@
-import { MAP_ROUTE } from "@constants/routes"
-import { lazy, Suspense } from "react"
-import { Route, Routes } from "react-router-dom"
+import { LOGIN_ROUTE, MAP_ROUTE, SIGNUP_ROUTE } from "@constants/routes";
+import { lazy, Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
 
 export const RouterAllRoutes = () => {
-  const Map = lazy(() => import("@pages/Home/home.page"))
+  const Map = lazy(() => import("@pages/Home/home.page"));
+  const Login = lazy(() => import("@pages/Login/login.page"));
 
   return (
     <main>
       <Suspense>
         <Routes>
-          <Route element={<Map />} path={MAP_ROUTE}/>
+          <Route element={<Login />} path={LOGIN_ROUTE} />
+          {/* <Route element={<Register />} path={SIGNUP_ROUTE} /> */}
+          <Route element={<Map />} path={MAP_ROUTE} />
         </Routes>
       </Suspense>
     </main>
-  )
-}
+  );
+};
