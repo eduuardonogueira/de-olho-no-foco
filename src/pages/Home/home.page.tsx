fb89d6f731330ba270259bfd2b67768d53fc0ff4
Eduardo Nogueira <eduardo.castro@compasso.com.br>
2024-10-31 14:09:14 -0300

    fix: loop flyTo userLocation

--- src/pages/Home/home.page.tsx
+++ src/pages/Home/home.page.tsx
@@ -1,22 +1,21 @@
 import { Menu, MyMap, SearchBar } from "@components/index";
 import styles from "./home.module.scss";
 import { MapContainer } from "react-leaflet";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import { Map as TypeMap } from "leaflet";
 import useUserLocation from "@hooks/useUserLocation";
 
 export const Home = () => {
   const mapRef: React.LegacyRef<TypeMap> | undefined = useRef(null);
   // const map = document.getElementById("mapContainer");
-  
-  const { userLocation } = useUserLocation() 
+
+  const { userLocation } = useUserLocation();
 
   useEffect(() => {
-   if (mapRef.current && userLocation) {
-    mapRef.current.flyTo(userLocation.coordinates, 15);
-   }
-  }, [])
-
+    if (mapRef.current && userLocation) {
+      mapRef.current.flyTo(userLocation.coordinates, 15);
+    }
+  }, [mapRef.current]);
 
   return (
     <div className={styles.container}>
@@ -25,7 +24,11 @@ export const Home = () => {
         <MapContainer
           ref={mapRef}
           id="mapContainer"
-          center={userLocation?.coordinates || [-1.4548981866300403, -48.44616551421902]}
+          center={
+            userLocation?.coordinates || [
+              -1.4548981866300403, -48.44616551421902,
+            ]
+          }
           zoom={15}
           className={styles.mapContainer}
           zoomControl={false}
