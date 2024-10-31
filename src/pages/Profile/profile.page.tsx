1b1ed55413333084de8a168dce95002ecb8362c4
eduardo castro <eduardocastroemp@gmail.com>
2024-10-31 13:34:49 -0300

    deploy

--- src/pages/Profile/profile.page.tsx
+++ src/pages/Profile/profile.page.tsx
@@ -3,7 +3,7 @@ import { Menu } from "@components/index"
 export const Profile = () => {
   return (
     <section>
-      <h1>Profile</h1>
+      <h1>Profile Page</h1>
       <Menu />
     </section>
   )
