4d2b143e33422390e33067034ba897b7b65cd2e0
lEduardoReis <beefarias15@gmail.com>
2024-11-18 08:59:49 -0300

    tela de perfil

---
+++ src/assets/index.ts
@@ -0,0 +1,6 @@
+export { default as IconMeusDados } from './icons_perfil/dados.png';
+export { default as IconHistorico } from './icons_perfil/historico.png';
+export { default as IconConfiguracoes } from './icons_perfil/configuracoes.png';
+export { default as IconConquistas } from './icons_perfil/conquistas.png';
+export { default as IconAjuda } from './icons_perfil/ajuda.png';
+export { default as IconSair } from './icons_perfil/sair.png';
